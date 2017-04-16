5162b1d05f6540a14de011b1253fa0674599b372
M.Meera Mohaideen <mohaideen.3mm@gmail.com>
2017-04-16 20:46:18 +0530

    Create train_reservation.h

---
+++ train_reservation.h
@@ -0,0 +1,11 @@
+#include<iostream>
+class reservation
+{
+        public:
+        void admin();
+        int register_user();
+        int login_user();
+        void booking_ticket();
+        void cancel();
+        void train_details();
+};
